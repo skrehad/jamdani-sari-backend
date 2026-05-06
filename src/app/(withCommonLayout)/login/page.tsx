d39f1714ac486919468bc91e92d0971119e4108f
rehadhasan <rehadhasan664@gmail.com>
2026-05-06 23:06:46 +0600

    slove

--- src/app/(withCommonLayout)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/module/auth/loginFrom";
-
-export default function LoginPage() {
-  return (
-    <div className="my-20">
-      <LoginForm />
-    </div>
-  );
-}
