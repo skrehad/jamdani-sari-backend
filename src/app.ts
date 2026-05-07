1161fc1503eb38d5dc21bc81c5085436533839b4
rehadhasan <rehadhasan664@gmail.com>
2026-05-08 03:09:05 +0600

    update express data limit

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,14 @@ const app = express();
 
 app.use(cookieParser());
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(
   cors({
     // origin: "http://localhost:3000",
     origin: "https://jamdani-frontend.vercel.app",
-     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
 );
